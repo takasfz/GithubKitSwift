74fafa9f30a8952edf8c2b0109ff6f7d12b57727
Takashi Kinjo <12707307+takasfz@users.noreply.github.com>
2017-11-22 20:10:25 +0900

    test

--- GithubKitTests/GithubKitTests.swift
+++ GithubKitTests/GithubKitTests.swift
@@ -9,11 +9,16 @@
 import XCTest
 @testable import GithubKit
 
+import RxSwift
+import RxTest
+import RxBlocking
+
 class GithubKitTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        ApiSession.shared.token = "Your Github Personal Access Token"
     }
     
     override func tearDown() {
@@ -21,6 +26,14 @@ class GithubKitTests: XCTestCase {
         super.tearDown()
     }
     
+    func test_search() {
+        let request = SearchUserRequest(query: "marty", after: nil, limit: 5)
+        let observable = ApiSession.shared.rx.send(request)
+        let result = try! observable.toBlocking().single()
+        
+        print("\(result)")
+    }
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
